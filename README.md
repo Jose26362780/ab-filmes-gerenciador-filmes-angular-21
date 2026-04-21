34843e8f27db9334c11267d7c1061ae27022f4f3
Jose Martinez <juniorjose1925@gmail.com>
2026-04-21 17:37:44 -0300

    Layout Base

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AbFilmesGerenciadorFilmesAngular21
+# GerenciadorFilmesTeste
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.0.
 
---
+++ src/app/features/authentication/layout/authentication-screen/authentication-screen.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-authentication-screen',
+  imports: [RouterOutlet],
+  templateUrl: './authentication-screen.html',
+
+})
+export class AuthenticationScreen {}
