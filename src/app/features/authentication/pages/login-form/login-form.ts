6a541587c4aa34b5b1965c4398819e30de05a1b6
Jose Martinez <juniorjose1925@gmail.com>
2026-04-22 21:43:36 -0300

    criar formulario de login e aplicar validacao de campos email e senha

--- src/app/features/authentication/pages/login-form/login-form.ts
+++ src/app/features/authentication/pages/login-form/login-form.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { email, form, minLength, required, Field } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [Field],
   templateUrl: './login-form.html',
 })
-export class LoginForm {}
+export class LoginForm {
+  loginModel = signal({
+    email: '',
+    password: '',
+  });
+
+  loginForm = form(this.loginModel, (fieldPath) => {
+    required(fieldPath.email, {
+      message: 'O E-mail é obrigatorio.',
+    });
+    email(fieldPath.email, { message: 'O E-mail está inválido.' });
+
+    required(fieldPath.password, { message: 'a Senha é obrigatória.' });
+    minLength(fieldPath.password, 8, { message: ' A seanha deve ter no minimo 8 caracteres.' });
+  });
+
+  
+}
