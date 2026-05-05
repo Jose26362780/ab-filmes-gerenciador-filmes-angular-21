0a36086b35bca8574d17175aa8f8854fae7bc526
Jose Martinez <juniorjose1925@gmail.com>
2026-05-05 20:00:57 -0300

    refator: Binding dos Detalhes do Filme

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -1,20 +1,31 @@
-import { Component, inject, input, signal, WritableSignal } from '@angular/core';
+import { Component, inject, input, linkedSignal, signal, WritableSignal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { MoviesApi } from '../../services/movies-api';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './movie-details.html',
 })
 export class MovieDetails {
   private readonly _moviesApi = inject(MoviesApi);
+
+  readonly BASE_URL = 'http://localhost:3000';
 
   id = input.required<string>();
 
   movieDetailsResource = rxResource({
     params: () => this.id(),
     stream: ({ params }) => this._moviesApi.getMovieDetails(+params),
+  });
+
+  movieDetails = linkedSignal(() => {
+    const ERROR_ON_RESPONSE = !!this.movieDetailsResource.error();
+
+    if (ERROR_ON_RESPONSE) return undefined;
+
+    return this.movieDetailsResource.value();
   });
 
   ////////////////////////
