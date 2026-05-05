04bb22f4f4d850273f3fbcf6d2525049f3fd98c5
Jose Martinez <juniorjose1925@gmail.com>
2026-05-04 21:53:51 -0300

    Listando Filmes Favoritos

--- src/app/features/favorites/pages/favorite-movies/favorite-movies.ts
+++ src/app/features/favorites/pages/favorite-movies/favorite-movies.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MoviesList } from '../../../../shared/components/movies-list/movies-list';
+import { FavoritesApi } from '../../../../shared/services/favorites-api';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-favorite-movies',
   imports: [MoviesList],
   templateUrl: './favorite-movies.html',
 })
-export class FavoriteMovies {}
+export class FavoriteMovies {
+  private readonly _favoritesApi = inject(FavoritesApi);
+
+  favoritesResource = rxResource({
+    params: () => true,
+    stream: () => this._favoritesApi.getFavorites(),
+  });
+
+  favoritesList = computed(() => {
+    const ERROR_ON_RESPONSE = !!this.favoritesResource.error();
+
+    if (ERROR_ON_RESPONSE) return [];
+
+    return this.favoritesResource.value() ?? [];
+  });
+}
